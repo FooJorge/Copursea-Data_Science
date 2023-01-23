9477481bd8ca0a2821da25a521fa857b69ff868b
FooJorge <117450327+FooJorge@users.noreply.github.com>
2023-01-23 14:03:42 +0100

    Update dash_layout.py

--- dash_layout.py
+++ dash_layout.py
@@ -28,7 +28,22 @@ app.layout = html.Div(children=[
                            ],
                   placeholder='Select a report type',
                   style={'width': '80%', 'padding': '3px', 'fontSize': '20px', 'textAlign':'center'})
+    # Add Computed graphs
+    # REVIEW3: Observe how we add an empty division and providing an id that will be updated during callback
+    html.Div([ ], id='plot1'),
     
+    html.Div([
+            html.Div([ ], id='plot2'),
+            html.Div([ ], id='plot3')
+    ], style={'display': 'flex'}),
+                                
+    # TASK3: Add a division with two empty divisions inside. See above disvision for example.
+    # Enter your code below. Make sure you have correct formatting.
+    html.Div([
+            html.Div([ ], id='plot4'),
+            html.Div([ ], id='plot5')
+    ], style={'display': 'flex'}),
+    ])
 ])
 # Run Application
 if __name__ == '__main__':
