ca9d998e049821093b3c3fb78467d9edf6386972
FooJorge <117450327+FooJorge@users.noreply.github.com>
2023-01-23 13:53:51 +0100

    Update dash_layout.py

--- dash_layout.py
+++ dash_layout.py
@@ -6,12 +6,11 @@ import dash_html_components as html
 import plotly.express as px
 from dash.dependencies import Input, Output
 
-# Add Dataframe
 
-# Add a bar graph figure
 
 app = dash.Dash()
 app.layout = html.Div(children=[
+    # TASK1: Add title to the dashboard
     html.H1(
         children='US Domestic Airline Flight Performance and Delay Statistics Dashboard',
         style={
@@ -21,17 +20,16 @@ app.layout = html.Div(children=[
         }
     ),
 
-    # Create dropdown
+    # TASK2: Add a dropdown
     dcc.Dropdown(id='input-type', 
                    options=[
                            {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
                            {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}
                            ],
                   placeholder='Select a report type',
-                  style={'width': '80%', 'padding': '3px', 'fontSize': '20px', 'text-align-last': 'center'})
-    # Bar graph
+                  style={'width': '80%', 'padding': '3px', 'fontSize': '20px', 'textAlign':'center'})
+    
 ])
-
 # Run Application
 if __name__ == '__main__':
     app.run_server()
